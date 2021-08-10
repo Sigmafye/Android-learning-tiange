8e5966e6affc6c97995fb812fa7bd64f283c098c
xutiel <xutie.fe@qq.com>
2021-08-10 11:09:39 +0800

    Done--Jump with diff launchMode

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# 安卓开发
-"tiange" form bilibili
+# Android develop learning
+> learn form Tiange
+## launchMode
+### standard
+> 标准模式，默认
+Activity是由任务栈管理的,每启动一个Activity ,就会被放入栈中,按返回键,就会从栈顶移除一个Activity。
+
+### singleTop
+> Task栈顶复用模式
+当要启动的目标Activity已经位于栈顶时,不会创建新的实例,会复用栈顶的Activity ,并且其onNewIntent()方法会被调用;如果目标Activity不在栈顶,则跟standard一样创建新的实例。
+
+### singleTask
+> Task栈内复用模式
+<b>Remember the taskAffinity should start with .</b>
+在同一个任务栈中,如果要启动的目标Activity已经在栈中,则会复用该Activity ,并调用其onNewIntent()方法,并且该Activity.上面的Activity会被清除;如果栈中没有,则创建新的实例。
+可通过`android:taskAffinity=".exampleAffinity"`实现任务栈的指定，任务栈默认是包名
+
+### singleInstance
+> 全局单例模式
+全局复用,不管哪个Task栈,只要存在目标Activity ,就复用。每个Activity占有一个新的Task栈。
+
+
--- app/src/main/java/com/example/matrix/jump/AActivity.java
+++ app/src/main/java/com/example/matrix/jump/AActivity.java
@@ -2,9 +2,12 @@ package com.example.matrix.jump;
 
 import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,13 +15,28 @@ import android.widget.Toast;
 import com.example.matrix.R;
 
 public class AActivity extends AppCompatActivity {
-    private Button mbtn_jump;
+    private Button mbtn_jump, mBtnJumpA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a);
+
+        Log.d("AActivity", "----onCreate-----");
+        Log.d("AActivity", "taskId: " + getTaskId() + ", hashcode:" + hashCode());
+        logTaskName();
+
         mbtn_jump = findViewById(R.id.btn_jump);
+        mBtnJumpA = findViewById(R.id.btn_jump_a);
+
+        mBtnJumpA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AActivity.this, AActivity.class);
+                startActivity(intent);
+            }
+        });
+
         mbtn_jump.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,8 +46,8 @@ public class AActivity extends AppCompatActivity {
                 bundle.putString("name", "西罗");
                 bundle.putInt("number", 9527);
                 intent.putExtras(bundle);
-//                startActivity( intent );
-                startActivityForResult(intent, 0);
+                startActivity(intent);
+//                startActivityForResult(intent, 0);
 
                 //显式二；
 //                Intent intent = new Intent();
@@ -55,9 +73,30 @@ public class AActivity extends AppCompatActivity {
         });
     }
 
+
     @Override
+    //startActivityForResult(intent, 0);配合使用
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         Toast.makeText(AActivity.this, data.getExtras().getString("return"), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    //在launchMode为singleTop时会被调用
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.d("AActivity", "----onNewIntent-----");
+        Log.d("AActivity", "taskId: " + getTaskId() + ", hashcode:" + hashCode());
+        logTaskName();
+    }
+
+    protected void logTaskName() {
+        try {
+            ActivityInfo info = getPackageManager().getActivityInfo(getComponentName(), PackageManager.GET_META_DATA);
+            //获取任务栈的名称
+            Log.d("AActivity", info.taskAffinity);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
